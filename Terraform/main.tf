0d8a2f34060934e6f02825deb760bafd416631f0
Tung-1991 <tungnt9905@gmail.com>
2023-11-04 13:05:41 +0700

    Final

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,43 +1,58 @@
+#File CORE của Terraform dùng để chạy các module tương tác với VCSA
+#xác thực kết nối với VM bằng các biến tự khởi tạo
 provider "vsphere" {
   user 			= var.vsphere_user
   password		= var.vsphere_password
   vsphere_server	= var.vsphere_server
   allow_unverified_ssl	= true
 }
+#gọi các tài nguyên trên VCSA với biến tự khởi tạo
+#Tên của Datacenter
 data "vsphere_datacenter" "dc" { name = var.vsphere_datacenter}
+#tên của các máy chủ ESXI
 data "vsphere_host" "hosts" {
   name			= var.vsphere_host
   datacenter_id		= data.vsphere_datacenter.dc.id
 }
+#tên của các cluster
 data "vsphere_compute_cluster" "compute_cluster" {
   name			= var.vsphere_compute_cluster
   datacenter_id		= data.vsphere_datacenter.dc.id
 }
+#tên của ổ lưu trữ
 data "vsphere_datastore" "datastore" {
   name			= var.vsphere_datastore
   datacenter_id         = data.vsphere_datacenter.dc.id
 }
+#tên của card mạng
 data "vsphere_network" "network" {
   name			= var.vsphere_network
   datacenter_id		= data.vsphere_datacenter.dc.id
 }
+#tên của template sẽ sử dụng để triển khai máy ảo
 data "vsphere_virtual_machine" "template" {
   name			= var.vm_template_name
   datacenter_id         = data.vsphere_datacenter.dc.id
 }
+#tạo các tài nguyên cần thiết cho máy ảo triển khai trên VCSA
 resource "vsphere_virtual_machine" "vm" {
+  #với mỗi vòng lặp var.vms sẽ tạo 1 máy ảo tương ứng
   for_each 		= var.vms
+  #khai báo các tài nguyên mà máy ảo sử dụng 
   datastore_id		= data.vsphere_datastore.datastore.id
   resource_pool_id	= data.vsphere_compute_cluster.compute_cluster.resource_pool_id
   guest_id		= var.vm_guest_id
+  #khai báo card mạng cho máy ảo
   network_interface {
     network_id 		= data.vsphere_network.network.id
     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
+  #khai báo tên, CPU,memory
   name          = each.value.name
   num_cpus 		= each.value.vcpu
   memory		= each.value.memory
   firmware		= var.vm_firmware 
+  #khai báo ổ cứng
   dynamic "disk" {
     for_each = var.disks
     content {
@@ -47,6 +62,7 @@ resource "vsphere_virtual_machine" "vm" {
       unit_number      = disk.value.unit_number
     }
   }
+  #clone của template và truyền các tham số vào như máy ảo ổ đĩa 
   clone {
     template_uuid       = data.vsphere_virtual_machine.template.id
     customize {
